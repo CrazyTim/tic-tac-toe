689257f90895d0ab739de34ed03cc4706c7312ed
CrazyTim <impdeveloper@gmail.com>
2019-08-03 22:44:42 +1000

    WIP: finish algorithm improvement, style improvements

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ function Square(props) {
 	if (props.winner) {
 		className += ' winner';
 	}
+
+	if (props.gameOver) {
+		className += ' noPointer'
+	}
+
 	return (
 	  <button 
 	  	className={className} 
@@ -17,17 +22,36 @@ function Square(props) {
 }
 
 function PlayAgainButton(props) {
-  let className = "play-again-button";
+  let className = '';
 	if (!props.visible) {
-		className += ' hidden';
+		className = ' hidden';
 	}
 
 	return (
-	  <button 
-	  	className={className}
-	  	onClick={props.onClick}>
-	    {props.value}
-	  </button>
+		<div className={'play-again' + className}>
+		  <button 
+		  	onClick={props.onClick}>
+		    {props.value}
+		  </button>
+	  </div>
+	);
+}
+
+function ScoreBoard(props) {
+
+	let className1 = '';
+	let className2 = '';
+	if (props.score.X > props.score.O) {
+		className1 = ' winner';
+	} else if (props.score.X < props.score.O) {
+		className2 = ' winner';
+	} 
+
+	return (
+		<div className="score-board">
+	  	<div className={"x" + className1}><span>Player X:</span><span>{props.score.X}</span></div>
+	  	<div className={"y" + className2}><span>Player O:</span><span>{props.score.O}</span></div>
+	  </div>
 	);
 }
 
@@ -35,44 +59,53 @@ class Board extends React.Component {
 
   renderSquare(i) {
     return (
-    	<Square 
+    	<Square
+    		key={i}
   			value={this.props.squares[i]} 
   			onClick={() => this.props.onClick(i)}
   			winner={this.props.winningSquares.includes(i)}
+  			gameOver={this.props.winningSquares.length>0}
   		/>
   	);
   }
 
-  renderRows(count,i) {
-
-  }
-
   render() {
-  	const rows = Array(this.props.rows);
-  	for (let i=0; i<this.props.rows; i++) {
-			const squares = Array(this.props.cols);
-			const startIndex = (i * this.props.cols);
-			for (let j=0; j<this.props.cols; j++) {
+
+  	const rows = Array(this.props.numRows);
+
+  	for (let i=0; i<this.props.numRows; i++) {
+			const squares = Array(this.props.numCols);
+			const startIndex = (i * this.props.numCols);
+			for (let j=0; j<this.props.numCols; j++) {
 				squares.push(this.renderSquare(startIndex+j));
 			}
-  		rows.push(<div className="board-row">{squares}</div>)
+  		rows.push(<div key={i} className="board-row">{squares}</div>)
   	}
-    return rows;
+
+    return <div>{rows}</div>;
+
   }
+
 }
 
 class Game extends React.Component {
 
 	constructor(props) {
+
+		const numRows = 7;
+		const numCols = 7;
+
 		super(props);
 		this.state = {
-			squares: Array(9).fill(null),
+			squares: Array(numRows * numCols).fill(null),
 			xIsNext: true,
 			winner: null,
 			draw: false,
-			winningSquares: Array(),
-			rows: 3,
-			cols: 3,
+			winningSquares: [],
+			numRows: numRows,
+			numCols: numCols,
+			numCellsInALineToWin: 3,
+			score: {'X':0, 'O':0}
 		};
 	}
 
@@ -88,7 +121,17 @@ class Game extends React.Component {
 		squares[i] = this.state.xIsNext ? 'X' : 'O';
 
 		// check for a winner
-		const [winner, winningSquares] = calculateWinner(squares);
+		let score = Object.assign({}, this.state.score);
+		let winner = null;
+		let winningSquares;
+		for (let i = 0; i < squares.length - 1; i++) {
+			winningSquares = checkWin(squares, this.state.numRows, this.state.numCols, i, this.state.numCellsInALineToWin);
+			if (winningSquares.length === this.state.numCellsInALineToWin) {
+				winner = squares[i];
+				score[winner] +=1;
+				break;
+			}
+		}
 
 		// check for draw
 		let draw = false;
@@ -102,16 +145,17 @@ class Game extends React.Component {
 			winner: winner,
 			winningSquares: winningSquares,
 			draw: draw,
+			score: score,
 		});
 
 	}
 
 	resetBoard() {
 		this.setState({
-			squares: Array(9).fill(null),
+			squares: Array(this.state.numRows * this.state.numCols).fill(null),
 			xIsNext: true,
 			winner: null,
-			winningSquares: Array(),
+			winningSquares: [],
 			draw: false,
 		});
 	}
@@ -130,22 +174,29 @@ class Game extends React.Component {
     return (
       <div className="game">
       	<div className="game-board">
-	        <Board 
+
+	    		<ScoreBoard
+						score={this.state.score}
+					/>
+
+					<Board 
 	        	squares={this.state.squares}
 	        	onClick={(i) => this.handleClick(i)}
 	        	winningSquares={this.state.winningSquares}
-	        	rows={this.state.rows}
-	        	cols={this.state.cols}
+	        	numRows={this.state.numRows}
+	        	numCols={this.state.numCols}
+	        	score={this.state.score}
 	        />
-	      </div>
-        <div className="game-info">
-          <div className="status">{status}</div>
-					<PlayAgainButton 
+
+	    		<div className="status">{status}</div>
+
+	    		<PlayAgainButton 
 						value="play again" 
 						onClick={() => this.resetBoard()}
 						visible={(this.state.winner || this.state.draw)}
 					/>
-        </div>
+
+    		</div> 
       </div>
     );
   }
@@ -158,81 +209,78 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-function calculateWinner(squares) {
-	const lines = [
-		[0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-	];
-	for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return [squares[a], lines[i]];
-    }
-  }
-  return [null, Array()];
-}
-
-function checkWin(squares, rows, cols, cell, numCellsInARow) {
-
+function checkWin(squares, numRows, numCols, cell, numCellsInALineToWin) {
 	// look in all directions around this cell to see if the player has won
 
+	if (squares[cell] === null) return [];
+
+	// define each x/y direction to search in
 	const directions = [
-		[ 0,-1], // north
-		[ 1,-1], // ne
-		[ 1, 0], // east
-		[ 1, 1], // se
-		[ 0, 1], // south
-		[-1, 1], // sw
-		[-1, 0], // west
-		[-1,-1], // nw
+		['n ', 0,-1],
+		['ne', 1,-1],
+		['e ', 1, 0],
+		['se', 1, 1],
+		['s ', 0, 1],
+		['sw',-1, 1],
+		['w ',-1, 0],
+		['nw',-1,-1],
 	];
 
+	// check in each direction for a match
 	for (let i = 0; i < directions.length; i++) {
-		const x = directions[i][0];
-		const y = directions[i][1];
-		const winningSquares = check(squares, rows, cols, cell, x, y, numCellsInARow);
-		if (winningSquares.length == numCellsInARow) return winningSquares; // player has won
+		const x = directions[i][1];
+		const y = directions[i][2];
+
+		//console.log('direction check: ' + directions[i][0] + " (x: " + directions[i][1] + ", y: " + directions[i][2] + ")");
+		
+		const winningSquares = check(squares, numRows, numCols, cell, x, y, numCellsInALineToWin);
+		if (winningSquares.length === numCellsInALineToWin) return winningSquares; // match found
 	}
 
 	return []; // no matches found
 	
 }
 
-function check(squares, rows, cols, cell, x, y, numCellsInARow) {
+function check(squares, numRows, numCols, cell, x, y, numCellsInALineToWin) {
 	// look in one direction, starting from cell, and staying within the bounds of the grid
 	// checking if every value is equal to the value in cell
+	// returns an array of the cells that match.
+	// ASSUMPTION: cell is correct, and fits inside the number of rows and cols
 
 	const v = squares[cell]; // the value we are trying to match
 
 	let found = [cell];	// array to store the matches
 
-	const xPos = cell % cols;
-	const xPosLast = xPos + (x * (numCellsInARow - 1));
-	if ( xPosLast > rows || xPosLast < 0 ) return []; // search would extend beyond the grid x bounds
+	// only looking for one match lol
+	if (numCellsInALineToWin === 1) return [cell];
+
+	// check if we would extend beyond the grid on x-axis
+	const thisCol = cell % numCols; // index
+	const lookAheadCol = thisCol + (x * (numCellsInALineToWin - 1));
+	if (lookAheadCol > (numCols-1) || lookAheadCol < 0) return [];
 	
-	// todo... 
-	//const yPos = nextCell / 
+
+	// check if we would extend beyond the grid on y-axis
+	const thisRow = Math.floor(cell / numRows); // index
+	const lookAheadRow = thisRow + (y * (numCellsInALineToWin - 1));
+	if (lookAheadRow > (numRows-1) || lookAheadRow < 0) return [];
+
+	//console.log('lookAheadCol: ' + lookAheadCol + ', lookAheadRow: ' + lookAheadRow);
 
 	let thisCell = cell;
-	for (let i = 0; i < numCellsInARow -1; i++) {
+	for (let i = 0; i < (numCellsInALineToWin - 1); i++) {
 
 		// calc index of next cell we need to check
-		thisCell = (x * 1) + (y * cols);
-
-		if (squares[thisCell] == v) {
+		thisCell += (x * 1) + (y * numCols);
+		//console.log('check cell: ' + thisCell);
+		if (squares[thisCell] === v) {
 			found.push(thisCell);
 		} else {
 			return [];
 		}
 		
 	}
-
-	return [found];
+	
+	return found;
 
 }
