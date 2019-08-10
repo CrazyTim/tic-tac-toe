8fcb85db6120e569bcb54ffb1ffbdd7572bfb371
CrazyTim <impdeveloper@gmail.com>
2019-08-10 23:05:15 +1000

    Remove unnecessary imports

--- src/components/Board.mjs
+++ src/components/Board.mjs
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 import Square from './Square.mjs';
 import './Board.css';
--- src/components/Game.mjs
+++ src/components/Game.mjs
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 import Board from './Board.mjs';
 import Button from './Button.mjs';
