6c017bc3c1abd300f23fb6f600fee1ff4e8cd300
CrazyTim <impdeveloper@gmail.com>
2020-05-01 00:18:42 +1000

    Show scores when > 0

--- src/components/Scoreboard.mjs
+++ src/components/Scoreboard.mjs
@@ -14,15 +14,18 @@ const Scoreboard = (props) => {
     'winner': props.score.X < props.score.O,
   });
 
+  const scoreX = props.score.X > 0 ? props.score.X : '\u00a0'; // blank space so div doesn't shrink
+  const scoreO = props.score.O > 0 ? props.score.O : '\u00a0'; // blank space so div doesn't shrink
+
   return (
     <div className='score-board'>
       <div className={classNameX}>
         <div>X</div>
-        <div>{props.score.X}</div>
+        <div>{scoreX}</div>
       </div>
       <div className={classNameY}>
         <div>O</div>
-        <div>{props.score.O}</div>
+        <div>{scoreO}</div>
       </div>
     </div>
   );
