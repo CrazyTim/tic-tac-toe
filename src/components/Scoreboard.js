85a53ae2d38936c5908821b806866a2bd937d1cd
CrazyTim <impdeveloper@gmail.com>
2020-08-18 21:41:23 +1000

    Display `-` when score is `0` instead of an empty space. - Looks better. - Still better than `0` because `0` looks like the `O` symbol above it.

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -14,8 +14,8 @@ const Scoreboard = (props) => {
     'winner': props.score.X < props.score.O,
   });
 
-  const scoreX = props.score.X > 0 ? props.score.X : '\u00a0'; // blank space so div doesn't shrink
-  const scoreO = props.score.O > 0 ? props.score.O : '\u00a0'; // blank space so div doesn't shrink
+  const scoreX = props.score.X > 0 ? props.score.X : '-'; // blank space so div doesn't shrink
+  const scoreO = props.score.O > 0 ? props.score.O : '-'; // blank space so div doesn't shrink
 
   return (
     <div className='score-board'>
